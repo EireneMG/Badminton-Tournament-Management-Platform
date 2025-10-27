45e6b40f00b41b518032f6959fb622bf4ae3f247
neohma <princesubido8@gmail.com>
2025-10-27 16:47:37 +0800

    manage player

--- BADMINTOUR/resources/views/dashboad-manager.blade.php
+++ BADMINTOUR/resources/views/dashboad-manager.blade.php
@@ -11,4 +11,10 @@
             <h3 class="text-lg font-semibold mb-4">Welcome, {{Auth:user()->name }}!</h3>
             <p class="text-gray-600 mb-4">You're logged in as a <span class="font-semibold text-indigo-600">Club Manager</span>.</p>
 
+            <div class="mt-6 grid grid -cols-1 md:grid-cols-2 gap-6">
+                <div class="bg-indigo-50 p-4 rounded-lg">
+                    <h4 class="font-semibold text-inidgo-900 mb-2">Manage Player</h4>
+                    <p class="text-sm text-indigo-700">View and manage all player in your club.</p>
+                </div>
+
 </div>
