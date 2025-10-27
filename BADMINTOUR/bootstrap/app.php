e51cac6c8309e0baf56a4bb72fec3b1917b06eb5
Laganas <kyrixkoone@gmail.com>
2025-10-27 17:17:51 +0800

    edit bootstrap app
    adding alias in bootstrap app

--- BADMINTOUR/bootstrap/app.php
+++ BADMINTOUR/bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'player' => \App\Http\Middleware\EnsureUserIsPlayer::class,
+            'manager' => \App\Http\Middleware\EnsureUserIsManager::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
