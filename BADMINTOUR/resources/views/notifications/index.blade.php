3a0ac384d42c2a2fc4de4d46086e2c85e9e07ee4
neohma <princesubido8@gmail.com>
2025-11-07 16:03:33 +0800

    Notif 3

--- BADMINTOUR/resources/views/notifications/index.blade.php
+++ BADMINTOUR/resources/views/notifications/index.blade.php
@@ -22,7 +22,6 @@
                             <path d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3z"/>
                         </svg>
                     </div>
-
                     <!-- Content -->
                     <div class="flex-1 min-w-0">
                         <div class="flex items-center justify-between">
@@ -46,7 +45,6 @@
                             <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
                         </svg>
                     </div>
-                    
                     <!-- Content -->
                     <div class="flex-1 min-w-0">
                         <div class="flex items-center justify-between">
@@ -57,6 +55,26 @@
                             </div>
                         </div>
                         <p class="text-sm text-gray-600 mt-1">Your match in Men's Singles has been rescheduled to tomorrow at 10:00 AM.</p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Notification Item 3 - Read -->
+            <div class="px-6 py-4 hover:bg-gray-50 border-b border-gray-200 transition">
+                <div class="flex items-start space-x-4">
+                    <!-- Icon -->
+                    <div class="flex-shrink-0 w-12 h-12 bg-[#2C5F4F] rounded-full flex items-center justify-center">
+                        <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                        </svg>
+                    </div>
+                    <!-- Content -->
+                    <div class="flex-1 min-w-0">
+                        <div class="flex items-center justify-between">
+                            <p class="text-base font-semibold text-gray-700">Match Victory!</p>
+                            <span class="text-sm text-gray-400">1d ago</span>
+                        </div>
+                        <p class="text-sm text-gray-600 mt-1">Congratulations! You won your match in the City Championship.</p>
                     </div>
                 </div>
             </div>
