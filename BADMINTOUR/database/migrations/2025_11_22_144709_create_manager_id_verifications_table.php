167c712f9386e900ae4b8a025c4077b8d0854f20
EireneMG <eirenegratuito@gmail.com>
2025-11-22 22:53:02 +0800

    Added database tables

--- BADMINTOUR/database/migrations/2025_11_22_144709_create_manager_id_verifications_table.php
+++ BADMINTOUR/database/migrations/2025_11_22_144709_create_manager_id_verifications_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('player')->after('email');
+        Schema::create('manager_id_verifications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
+        Schema::dropIfExists('manager_id_verifications');
     }
 };
