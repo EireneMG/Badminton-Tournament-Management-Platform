70f15af65ede5814d49dfc57d69d1324db359c58
Laganas <kyrixkoone@gmail.com>
2025-11-24 16:07:52 +0800

    Update MatchResult.php

--- BADMINTOUR/app/Models/MatchResult.php
+++ BADMINTOUR/app/Models/MatchResult.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MatchResult extends Model
 {
-    //
+    protected $fillable = [
+        'match_id',
+        'winner_id',
+        'player1_set1_score',
+        'player2_set1_score',
+        'player1_set2_score',
+        'player2_set2_score',
+        'player1_set3_score',
+        'player2_set3_score',
+        'score_inputted_by',
+        'inputted_by_user_id',
+    ];
+
+    public function match(): BelongsTo
+    {
+        return $this->belongsTo(TournamentMatch::class, 'match_id');
+    }
+
+    public function winner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'winner_id');
+    }
+
+    public function inputtedByUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'inputted_by_user_id');
+    }
 }
