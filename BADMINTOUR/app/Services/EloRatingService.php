8d970843fc0d2c663565d4ba43769070bdc5e438
Eirene <eirenegratuito@gmail.com>
2025-11-26 01:25:28 +0800

    Update EloRatingService.php
    Calculate doubles match ratings

--- BADMINTOUR/app/Services/EloRatingService.php
+++ BADMINTOUR/app/Services/EloRatingService.php
@@ -31,4 +31,39 @@ class EloRatingService
         $this->saveRankingHistory($player1, $categoryType, $player1OldRating, $player1NewRating);
         $this->saveRankingHistory($player2, $categoryType, $player2OldRating, $player2NewRating);
     }
+
+    public function calculateDoublesMatchRatings(User $team1Player1, User $team1Player2, User $team2Player1, User $team2Player2, bool $team1Won, string $categoryType): void
+    {
+        $team1Player1OldRating = $this->getCurrentRating($team1Player1, $categoryType);
+        $team1Player2OldRating = $this->getCurrentRating($team1Player2, $categoryType);
+        $team2Player1OldRating = $this->getCurrentRating($team2Player1, $categoryType);
+        $team2Player2OldRating = $this->getCurrentRating($team2Player2, $categoryType);
+        
+        $team1Rating = ($team1Player1OldRating + $team1Player2OldRating) / 2;
+        $team2Rating = ($team2Player1OldRating + $team2Player2OldRating) / 2;
+
+        $team1ExpectedScore = $this->calculateExpectedScore($team1Rating, $team2Rating);
+        $team2ExpectedScore = 1 - $team1ExpectedScore;
+        
+        $team1ActualScore = $team1Won ? 1 : 0;
+        $team2ActualScore = $team1Won ? 0 : 1;
+        
+        $team1RatingChange = self::K_FACTOR * ($team1ActualScore - $team1ExpectedScore);
+        $team2RatingChange = self::K_FACTOR * ($team2ActualScore - $team2ExpectedScore);
+
+        $team1Player1NewRating = round($team1Player1OldRating + $team1RatingChange);
+        $team1Player2NewRating = round($team1Player2OldRating + $team1RatingChange);
+        $team2Player1NewRating = round($team2Player1OldRating + $team2RatingChange);
+        $team2Player2NewRating = round($team2Player2OldRating + $team2RatingChange);
+
+        $this->updateRating($team1Player1, $categoryType, $team1Player1NewRating);
+        $this->updateRating($team1Player2, $categoryType, $team1Player2NewRating);
+        $this->updateRating($team2Player1, $categoryType, $team2Player1NewRating);
+        $this->updateRating($team2Player2, $categoryType, $team2Player2NewRating);
+        
+        $this->saveRankingHistory($team1Player1, $categoryType, $team1Player1OldRating, $team1Player1NewRating);
+        $this->saveRankingHistory($team1Player2, $categoryType, $team1Player2OldRating, $team1Player2NewRating);
+        $this->saveRankingHistory($team2Player1, $categoryType, $team2Player1OldRating, $team2Player1NewRating);
+        $this->saveRankingHistory($team2Player2, $categoryType, $team2Player2OldRating, $team2Player2NewRating);
+    }
 }
