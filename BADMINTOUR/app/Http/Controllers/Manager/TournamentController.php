b4937d4de82a4a1bc0b537ba995a41414e5b1a82
chloegndrl <mejoradac45@gmail.com>
2025-11-25 18:56:17 +0800

    Update TournamentController.php
    Constructor with MatchGenerationService injection

--- BADMINTOUR/app/Http/Controllers/Manager/TournamentController.php
+++ BADMINTOUR/app/Http/Controllers/Manager/TournamentController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreTournamentRequest;
+use App\Http\Requests\UpdateTournamentRequest;
+use App\Models\Tournament;
+use App\Models\TournamentCategory;
+use App\Models\Club;
+use App\Models\Notification;
+use App\Services\MatchGenerationService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class TournamentController extends Controller
 {
-    //
+    protected $matchGenerationService;
+
+    public function __construct(MatchGenerationService $matchGenerationService)
+    {
+        $this->matchGenerationService = $matchGenerationService;
+    }
 }
