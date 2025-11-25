b79763357179e3acf0dcf6b143c0ae58cf81c7ad
Eirene <eirenegratuito@gmail.com>
2025-11-25 15:14:09 +0800

    Update MatchController.php

--- BADMINTOUR/app/Http/Controllers/Manager/MatchController.php
+++ BADMINTOUR/app/Http/Controllers/Manager/MatchController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers\Manager;
 
 use App\Http\Controllers\Controller;
+use App\Models\TournamentMatch;
+use App\Models\MatchResult;
+use App\Models\Notification;
+use App\Services\EloRatingService;
+use App\Services\MatchGenerationService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class MatchController extends Controller
 {
