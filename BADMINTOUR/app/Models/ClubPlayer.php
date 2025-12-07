ef1454c77014f3e3e31ca216e1f65e47d765c296
Laganas <kyrixkoone@gmail.com>
2025-12-07 22:40:58 +0800

    update clubplayer and partner invitation

--- BADMINTOUR/app/Models/ClubPlayer.php
+++ BADMINTOUR/app/Models/ClubPlayer.php
@@ -14,6 +14,7 @@ class ClubPlayer extends Model
         'request_type',
         'skill_level',
         'provisional_elo',
+        'is_provisional',
     ];
 
     public function club(): BelongsTo
